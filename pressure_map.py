eab61a6bb997cde87d417c9f63611a8a23162011
polochinoc <polochinoc@gmail.com>
2017-04-29 19:22:53 +0200

    black and white pressure map

--- pressure_map.py
+++ pressure_map.py
@@ -125,22 +125,38 @@ def pressure_map():
     tab = np.linspace(0, 1, M)
 
         # Upper Curves
-    for l in tab:
-        Y = [((1 - l)*ey[i] + l*3*hmax) for i in range(Ne)]
-        curve_l = curve_length(ex, Y)
-        plt.plot(ex, Y, c=str((curve_l-1)*22))
+    Y1 = [((1 - tab[0])*ey[i] + tab[0]*3*hmax) for i in range(Ne)]
+    for j in range(M):
+        Ye = [((1 - tab[j])*ey[i] + tab[j]*3*hmax) for i in range(Ne)]
+        curve_l = curve_length(ex, Ye)
+        #print(curve_l)
+        plt.fill_between(ex, Y1, Ye, color=str((curve_l-1)*22))
+        plt.plot(ex, Ye, c=str((curve_l-1)*22))
+        Y1 = Ye
 
     Q = int(input("How many lower airflow curves do you wish to plot ?\t"))
     tab_2 = np.linspace(0, 1, Q)
-
+  
         # Lower curves
-    for l in tab_2:
-        Y = [((1 - l)*iy[i] + l*3*hmin) for i in range(Ni)]
-        curve_l = curve_length(ix, Y)
-        plt.plot(ix, Y, c=str((curve_l-1)*22))
+    Y2 = [((1 - tab_2[0])*iy[i] + tab_2[0]*3*hmin) for i in range(Ni)]
+    for j in range(Q):
+        Yi = [((1 - tab_2[j])*iy[i] + tab_2[j]*3*hmin) for i in range(Ni)]
+        curve_l = curve_length(ix, Yi)
+        #print(curve_l)
+        plt.fill_between(ix, Y2, Yi, color=str((curve_l-1)*22))
+        plt.plot(ix, Yi, c=str((curve_l-1)*22))
+        Y2 = Yi
 
     plt.plot(ex, ey, c="black", label="extrados")
     plt.plot(ix, iy, c="black", label="intrados")
+    Iy = np.zeros(len(ey))
+    for i in range(int((len(iy) - 1)/2)):
+        Iy[i] = iy[i]
+    for i in range(int((len(iy) - 1)/2), len(iy) - 1):
+        Iy[i] = iy[i] - 0.0038
+    Iy[len(iy) - 1] = Iy[len(iy) - 2]/2
+    Iy[len(iy)] = 0
+    plt.fill_between(ex, ey, Iy, color='black')
     plt.xlabel("x")
     plt.ylabel("y")
     plt.grid(True)
